cd585a5f54027a1d28c7a9ace618cf4a80a76ce6
Farizal Hamami <farizalhamami@gmail.com>
2019-10-12 23:09:54 +0700

    Add main variable file

---
+++ variable/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	// We have to initialize type of data in the end of line
+	var one int
+	one = 1
+
+	// A different way to create a variable
+	two := 2
+
+	fmt.Println(one)
+	fmt.Println(two)
+}
