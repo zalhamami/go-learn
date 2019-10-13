34b91edae3636224d420f85d5eb768ffe20ef70a
Farizal Hamami <farizalhamami@gmail.com>
2019-10-13 10:17:25 +0700

    Modify http file

--- interface/http.go
+++ interface/http.go
@@ -2,15 +2,22 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
 
 func main() {
-	response, err := http.Get("https://farizal.dummy.heikaku.com/v1/category")
+	response, err := http.Get("https://google.com")
 	if err != nil {
 		fmt.Println("Error:", err)
 		os.Exit(1)
 	}
-	fmt.Println(response)
+
+	// bs := make([]byte, 99999)
+	// response.Body.Read(bs)
+	// fmt.Println(string(bs))
+
+	// The simple way
+	io.Copy(os.Stdout, response.Body)
 }
